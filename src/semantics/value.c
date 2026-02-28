368cc5561ea08743e9357b6e863342b491b3a669
at2er <xb0515@outlook.com>
2026-02-28 12:49:49 +0800

    fix: libmcb submodule

--- src/semantics/value.c
+++ src/semantics/value.c
@@ -8,6 +8,19 @@
 #include "../type.h"
 #include "../value.h"
 
+static int analyse_elem_init_value(
+		struct zako_elem_init_value *elem_init,
+		struct zako_type *expect_type);
+
+int
+analyse_elem_init_value(
+		struct zako_elem_init_value *elem_init,
+		struct zako_type *expect_type)
+{
+	assert(elem_init && expect_type);
+	return 0;
+}
+
 enum ANALYSIS_RESULT
 analyse_value(struct zako_value *value, struct zako_type *expect_type)
 {
@@ -15,7 +28,12 @@ analyse_value(struct zako_value *value, struct zako_type *expect_type)
 	assert(value && expect_type);
 	switch (value->kind) {
 	case ELEM_INIT_VALUE:
-		break; //TODO
+		ret = analyse_elem_init_value(
+				&value->data.elem_init,
+				expect_type);
+		if (ret)
+			return ret;
+		break;
 	case EXPR_VALUE:
 		ret = analyse_expr(value->data.expr, expect_type);
 		if (ret)
